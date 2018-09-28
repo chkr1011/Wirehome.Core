66ec3bc2a72524a3f6dbc43fba520f22f56101fa
Christian Kratky <christian.kratky@googlemail.com>
2018-09-28 22:42:40 +0200

    Fix startup scripts (direct start and daemon).

--- Wirehome.Core.Hosts.Console/run.sh
+++ Wirehome.Core.Hosts.Console/run.sh
@@ -1,2 +1,4 @@
-﻿#!/bin/bash
-sudo dotnet ./Wirehome.Core.Hosts.Console.dll
+﻿#!/bin/sh
+
+echo "Starting Wirehome.Core..."
+dotnet ./Wirehome.Core.Hosts.Console.dll
--- Wirehome.Core.Hosts.Console/rund.sh
+++ Wirehome.Core.Hosts.Console/rund.sh
@@ -1,2 +1,9 @@
-﻿#!/bin/bash
-screen -S main -X bla $'sudo dotnet ./Wirehome.Core.Hosts.Console.dll\n'
+﻿#!/bin/sh
+
+echo "Setting up screen..."
+screen -dmS main
+screen -S main -X title Wirehome.Core
+screen -S main -X chdir /opt/wirehome
+
+echo "Starting Wirehome.Core..."
+screen -S main -X exec ./run.sh
