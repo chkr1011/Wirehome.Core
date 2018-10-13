3a2fbf9bdbf5fb465efa9e81c735fd0bc7dfae1b
Christian Kratky <christian.kratky@googlemail.com>
2018-10-13 21:37:15 +0200

    Add EF based DB for history data.

---
+++ Wirehome.Core/History/Repository/Entities/ComponentStatusEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Wirehome.Core.History.Repository.Entities
+{
+    [Table("ComponentStatus")]
+    public class ComponentStatusEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public uint ID { get; set; } 
+
+        [StringLength(256)]
+        public string ComponentUid { get; set; }
+
+        [StringLength(256)]
+        public string StatusUid { get; set; }
+
+        [StringLength(1024)]
+        public string Value { get; set; }
+
+        public DateTimeOffset RangeStart { get; set; }
+
+        public DateTimeOffset RangeEnd { get; set; }
+
+        public bool IsLatest { get; set; }
+
+        public uint? PredecessorID { get; set; }
+
+        [ForeignKey("PredecessorID")]
+        public ComponentStatusEntity Predecessor { get; set; }
+    }
+}
---
+++ Wirehome.Core/History/Repository/Entities/HistoryDatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Wirehome.Core.History.Repository.Entities
+{
+    public class HistoryDatabaseContext : DbContext
+    {
+        public HistoryDatabaseContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<ComponentStatusEntity> ComponentStatus { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ComponentStatusEntity>()
+                .HasIndex(b => new { b.RangeStart, b.RangeEnd, b.ComponentUid, b.StatusUid });
+        }
+    }
+}
