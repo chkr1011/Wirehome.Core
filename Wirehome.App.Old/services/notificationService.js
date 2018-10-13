8262484357f943d0ed73c5ea4c837a9134b3fc75
Christian Kratky <christian.kratky@googlemail.com>
2018-10-13 14:28:15 +0200

    Remove old web app.

--- Wirehome.App.Old/services/notificationService.js
+++
@@ -1,9 +0,0 @@
-﻿function createNotificationService(apiService) {
-    var srv = this;
-
-    srv.delete = function (notificationId) {
-        apiService.deleteNotification(notificationId);
-    };
-
-    return this;
-}
