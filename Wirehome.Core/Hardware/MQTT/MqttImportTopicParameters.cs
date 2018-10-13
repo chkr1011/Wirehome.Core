b814ed4ab778f1110d6326187412921171a31c6b
Christian Kratky <christian.kratky@googlemail.com>
2018-10-13 21:44:09 +0200

    Add support for credentials in topic import.

--- Wirehome.Core/Hardware/MQTT/MqttImportTopicParameters.cs
+++ Wirehome.Core/Hardware/MQTT/MqttImportTopicParameters.cs
@@ -8,7 +8,15 @@ namespace Wirehome.Core.Hardware.MQTT
 
         public int? Port { get; set; }
 
+        public string ClientId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
         public string Topic { get; set; }
+
+        public bool UseTls { get; set; }
 
         public MqttQualityOfServiceLevel QualityOfServiceLevel { get; set; } = MqttQualityOfServiceLevel.AtMostOnce;
     }
