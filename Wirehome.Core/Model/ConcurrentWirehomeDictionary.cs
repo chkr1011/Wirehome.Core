74125c74b5ee53dbde55eb952393d5f56a21a14f
Christian Kratky <christian.kratky@googlemail.com>
2018-10-17 00:45:48 +0200

    Add ConcurrentWirehomeDictionary.

---
+++ Wirehome.Core/Model/ConcurrentWirehomeDictionary.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Concurrent;
+using IronPython.Runtime;
+using Wirehome.Core.Python;
+
+namespace Wirehome.Core.Model
+{
+    public class ConcurrentWirehomeDictionary : ConcurrentDictionary<string, object>
+    {
+        public static implicit operator ConcurrentWirehomeDictionary(PythonDictionary pythonDictionary)
+        {
+            return PythonConvert.ToConcurrentWirehomeDictionary(pythonDictionary);
+        }
+
+        public static implicit operator PythonDictionary(ConcurrentWirehomeDictionary wirehomeDictionary)
+        {
+            return PythonConvert.ToPythonDictionary(wirehomeDictionary);
+        }
+    }
+}
