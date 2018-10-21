be864f2409710a21aac398bb99f90f0f1fde3611
Christian Kratky <christian.kratky@googlemail.com>
2018-10-21 20:30:37 +0200

    Add python proxy for serializing Python values.

--- Wirehome.Core/Python/Proxies/JsonSerializerPythonProxy.cs
+++ Wirehome.Core/Python/Proxies/JsonSerializerPythonProxy.cs
@@ -2,6 +2,8 @@
 // ReSharper disable InconsistentNaming
 // ReSharper disable UnusedMember.Global
 
+using System;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace Wirehome.Core.Python.Proxies
@@ -10,11 +12,30 @@ namespace Wirehome.Core.Python.Proxies
     {
         public string ModuleName { get; } = "json_serializer";
 
-        public object deserialize_json(object source)
+        public object deserialize(object value)
         {
-            var jsonText = new ConverterPythonProxy().to_string(source);
+            var jsonText = new ConverterPythonProxy().to_string(value);
             var json = JToken.Parse(jsonText);
+
             return PythonConvert.ToPython(json);
+        }
+
+        public string serialize(object value)
+        {
+            var json = PythonConvert.FromPythonToJson(value);
+            return json.ToString(Formatting.None);
+        }
+
+        public string serialize_indented(object value)
+        {
+            var json = PythonConvert.FromPythonToJson(value);
+            return json.ToString(Formatting.Indented);
+        }
+
+        [Obsolete]
+        public object deserialize_json(object value)
+        {
+            return deserialize(value);
         }
     }
 }
