bd8981d8acf1605ec5f1ad06379aaed9fb123587
Christian Kratky <christian.kratky@googlemail.com>
2018-11-04 00:10:06 +0100

    Optimize performance of JSON serializer.

--- Wirehome.Core/Storage/JsonSerializerService.cs
+++ Wirehome.Core/Storage/JsonSerializerService.cs
@@ -1,35 +1,53 @@
 ﻿using System;
+using System.IO;
 using System.Text;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 
 namespace Wirehome.Core.Storage
 {
     public class JsonSerializerService
     {
-        private readonly JsonSerializerSettings _jsonSerializerSettings = new JsonSerializerSettings
+        private readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
         {
-            DateParseHandling = DateParseHandling.None,
-            Formatting = Formatting.Indented
+            ContractResolver = new DefaultContractResolver
+            {
+                NamingStrategy = new CamelCaseNamingStrategy()
+            },
+            Formatting = Formatting.Indented,
+            DateParseHandling = DateParseHandling.None
         };
+
+        private readonly JsonSerializer _serializer;
+
+        public JsonSerializerService()
+        {
+            _serializer = JsonSerializer.Create(_serializerSettings);
+        }
 
         public string Serialize(object value)
         {
-            return JsonConvert.SerializeObject(value, _jsonSerializerSettings);
+            return JsonConvert.SerializeObject(value, _serializerSettings);
         }
 
         public TValue Deserialize<TValue>(string json)
         {
             if (json == null) throw new ArgumentNullException(nameof(json));
 
-            return JsonConvert.DeserializeObject<TValue>(json, _jsonSerializerSettings);
+            using (var streamReader = new StringReader(json))
+            {
+                return (TValue)_serializer.Deserialize(streamReader, typeof(TValue));
+            }
         }
 
         public TValue Deserialize<TValue>(byte[] json)
         {
             if (json == null) throw new ArgumentNullException(nameof(json));
 
-            var buffer = Encoding.UTF8.GetString(json);
-            return Deserialize<TValue>(buffer);
+            using (var streamReader = new StreamReader(new MemoryStream(json), Encoding.UTF8))
+            {
+                return (TValue)_serializer.Deserialize(streamReader, typeof(TValue));
+            }
         }
     }
 }
