f6562b2fedf5c4e93558413d5deaa3c80f26e240
Christian Kratky <christian.kratky@googlemail.com>
2018-12-22 09:52:04 +0100

    Add API which generates Wirehome Python API reference.

---
+++ Wirehome.Core/HTTP/Controllers/PythonApiController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Wirehome.Core.Python;
+
+namespace Wirehome.Core.HTTP.Controllers
+{
+    public class PythonApiController : Controller
+    {
+        private readonly PythonProxyFactory _pythonProxyFactory;
+
+        public PythonApiController(PythonProxyFactory pythonProxyFactory)
+        {
+            _pythonProxyFactory = pythonProxyFactory ?? throw new ArgumentNullException(nameof(pythonProxyFactory));
+        }
+
+        // TODO: Add reference in JSON format.
+
+        [HttpGet]
+        [Route("/api/v1/python_api/reference_document")]
+        [ApiExplorerSettings(GroupName = "v1")]
+        public string GetReferenceDocument()
+        {
+            var reference = new PythonProxyReferenceGenerator(_pythonProxyFactory).Generate();
+            HttpContext.Response.ContentType = "text/plain";
+            return reference;
+        }
+    }
+}
