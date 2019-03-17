4e252fc7343b2a7a99a1104524b06d9d1b732e1e
Christian Kratky <christian.kratky@googlemail.com>
2019-03-17 19:20:25 +0100

    Add setup project for Wirehome.Core.

---
+++ Wirehome.Core.Setup/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Wirehome.Core.Setup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
