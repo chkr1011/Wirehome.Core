3b0fbb1be4eb6d4b154c451eb757e13be85ec001
Christian Kratky <christian.kratky@googlemail.com>
2019-03-19 19:08:46 +0100

    Remove Wirehome.App (moved to Packages)

--- Wirehome.App/services/notificationService.js
+++
@@ -1,9 +0,0 @@
-﻿function createNotificationService(apiService) {
-    var srv = this;
-
-    srv.delete = function (notificationUid) {
-        apiService.executeDelete("/api/v1/notifications/" + notificationUid);
-    };
-
-    return this;
-}
